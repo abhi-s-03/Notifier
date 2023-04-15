3a61cc6a3f5752d3234cf1f47219dd6958f40bae
MVK2803 <mathewvinukariath@gmail.com>
2023-04-15 14:48:35 +0530

    :)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import Login from './components/Login/Login';
 
 import NewUser from './components/NewUser/NewUser';
+import Landing from './components/Landing/Landing';
 
 
 function App() {
