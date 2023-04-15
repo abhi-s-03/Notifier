fc628fcbf58a2fe7c7d8b8b7a7747af3cf9b133f
MVK2803 <mathewvinukariath@gmail.com>
2023-04-15 13:35:51 +0530

    Initial Commit

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+import './App.css';
+import Login from './components/Login/Login';
+
+import NewUser from './components/NewUser/NewUser';
+
+
+function App() {
+  return (
+    <div>
+      <NewUser/>
+    </div>
+  );
+}
+
+export default App;
