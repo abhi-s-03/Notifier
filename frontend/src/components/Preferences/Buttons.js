31edcd1334c826714dcf5da04405d91276598f06
MVK2803 <mathewvinukariath@gmail.com>
2023-04-17 00:04:50 +0530

    minor

--- frontend/src/components/Preferences/Buttons.js
+++ frontend/src/components/Preferences/Buttons.js
@@ -1,78 +1,4 @@
 export const modes = [
-<<<<<<< HEAD
-    {
-      id: "1",
-      label: "Whatsapp",
-      description: "Receive messages via Whatsapp only",
-    },
-    {
-      id: "2",
-      label: "In-App Notification",
-      description: "Receive messages via In-App Notification only",
-    },
-    {
-      id: "3",
-      label: "Both",
-      description: "Receive messages via both Whatsapp and In-App Notification",
-    },
-  ];
-
- export const ages = [
-    {
-      id: "a",
-      label: "0-12",
-      description: "Age range between 10-20 years old",
-    },
-    {
-      id: "b",
-      label: "13-19",
-      description: "Age range between 20-40 years old",
-    },
-    {
-      id: "c",
-      label: "20+",
-      description: "Age range between 40-60 years old",
-    }
-    
-  ];
-
-  export const times = [
-    {
-      id: "A",
-      label: "7-10 am",
-      description: "Receive messages between 7-10 am",
-    },
-    {
-      id: "B",
-      label: "10-12 am",
-      description: "Receive messages between 10-12 am",
-    },
-    {
-      id: "C",
-      label: "12-4 pm",
-      description: "Receive messages between 12-4 pm",
-    },
-    {
-      id: "D",
-      label: "6-12 am",
-      description: "Receive messages between 6-12 am",
-    },
-  ];
-
-  export const contents = [
-    {
-      id: "dep",
-      label: "Depression",
-      description: "Receive Depression Videos",
-    },
-    
-    {
-      id: "all",
-      label: "All ",
-      description: "All of the Above",
-    },
-  ];
-=======
   {
     id: "1",
     label: "Email",
@@ -145,4 +71,3 @@ export const contents = [
     description: "Get resources like books, movies and podcasts for self-help",
   },
 ];
->>>>>>> 1a44205a6739be4a902ec726a20a783eca049515
